c03d9acd86f9d3cf804070ea39a5f0e2b2097af6
Oleksandr102 <65391094+Oleksandr102@users.noreply.github.com>
2020-07-24 14:45:52 +0300

    Initial commit

---
+++ src/main/java/com/company/Execute.java
@@ -0,0 +1,12 @@
+package main.java.com.company;
+
+
+public class Execute {
+
+    public void call() {
+        int[] matrix = new int[Integer.MAX_VALUE];
+
+        for (int i = 0; i < matrix.length; i++)
+            matrix[i] = i + 1;
+    }
+}
